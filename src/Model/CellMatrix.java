fa306dc9429fbc0b1f47eae38aa45102702d2a76
xzc <984851056@qq.com>
2018-06-09 16:33:19 +0800

    x z c gwj

--- src/Model/CellMatrix.java
+++ src/Model/CellMatrix.java
@@ -19,48 +19,45 @@ public class CellMatrix {
     }
 
     public void LifeRule() {
-        int[][] tempMatrix=new int[heigh][weight];
         for (int i = 0; i < weight; i++)
             for (int j = 0; j < heigh; j++) {
-                tempMatrix[i][j]=0;
-                int num=AroundCellNumbers(i,j);
+                int num = AroundCellNumbers(i, j);
                 if (num == 3) {
-                    tempMatrix[i][j] = 1;
-                }
-                else if(num==2){
-                    tempMatrix[i][j] = cell[i][j];
+                    cell[i][j] = 1;
+                } else if (num < 2 || num > 3) {
+
+                    cell[i][j] = 0;
                 }
             }
-        cell=tempMatrix;
     }
 
     public int AroundCellNumbers(int x, int y) {
         int count = 0;
 
         //上方
-        if (x != 0)
-            count += cell[x-1][y ];
-        //下方
-        if (x != heigh - 1)
-            count += cell[x+1][y];
-        //左方
         if (y != 0)
-            count += cell[x][y-1];
+            count += cell[x][y - 1];
+        //下方
+        if (y != heigh - 1)
+            count += cell[x][y + 1];
+        //左方
+        if (x != 0)
+            count += cell[x - 1][y];
         //右方
-        if (y != weight - 1)
-            count += cell[x][y+1];
+        if (x != weight - 1)
+            count += cell[x + 1][y];
         //右下
-        if (y != weight - 1 && x != heigh - 1)
+        if (x != weight - 1 && y != heigh - 1)
             count += cell[x + 1][y + 1];
         //左上
         if (x != 0 && y != 0)
             count += cell[x - 1][y - 1];
         //右上
-        if (y != weight - 1 && x != 0)
-            count += cell[x -1][y + 1];
-        //左下
-        if (x != heigh - 1 && y != 0)
+        if (x != weight - 1 && y != 0)
             count += cell[x + 1][y - 1];
+        //左下
+        if (y != heigh - 1 && x != 0)
+            count += cell[x - 1][y + 1];
 
         return count;
     }
--- src/VIew/GameFrame.java
+++ src/VIew/GameFrame.java
@@ -4,47 +4,20 @@ import Model.CellMatrix;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.security.PrivateKey;
 
 public class GameFrame extends JFrame {
     private JTextField[][] celltext;
     private JPanel panel;
     private CellMatrix cellMatrix;
-    private JButton start=new JButton("Start");
-    private JButton pause=new JButton("pause");
-    private JLabel weigth=new JLabel("长度");
-    private JTextField weighttext=new JTextField();
-    private JLabel height=new JLabel("宽度");
-    private JTextField heighttext=new JTextField();
-    private JLabel rate=new JLabel("时间间隔");
-    private JTextField ratetext=new JTextField();
-    private JPanel controlpanel=new JPanel();
-    private boolean isStart=false;
 
     public GameFrame(CellMatrix cellMatrix) {
         setTitle("Life Game");
-        start.addActionListener(new StartGame());
-        pause.addActionListener(new PauseGame());
         this.setSize(1000, 1200);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         start();
         this.cellMatrix = cellMatrix;
         this.setVisible(true);
         add("Center", panel);
-        controlpanel=new JPanel();
-        controlpanel.setLayout(new GridLayout(1, 8));
-        controlpanel.add(start);
-        controlpanel.add(pause);
-        controlpanel.add(weigth);
-        controlpanel.add(weighttext);
-        controlpanel.add(height);
-        controlpanel.add(heighttext);
-        controlpanel.add(rate);
-        controlpanel.add(ratetext);
-
-        add(controlpanel, BorderLayout.NORTH);
     }
 
     public void start() {
@@ -56,29 +29,13 @@ public class GameFrame extends JFrame {
         for (int y = 0; y < rows; y++) {
             for (int x = 0; x < cols; x++) {
                 JTextField text = new JTextField();
-                text.setEditable(false);
-                text.setBorder(BorderFactory.createRaisedBevelBorder());
                 celltext[y][x] = text;
                 panel.add(text);
             }
         }
-    }
-    private class StartGame implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            isStart=true;
-        }
-    }
-    private class PauseGame implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            isStart=false;
-        }
+
     }
 
-    public boolean getstatus(){
-        return isStart;
-    }
     public void loadCell() {
         for (int i = 0; i < celltext.length; i++) {
             for (int j = 0; j < celltext[0].length; j++) {
@@ -89,6 +46,5 @@ public class GameFrame extends JFrame {
                 }
             }
         }
-        panel.updateUI();
     }
 }
